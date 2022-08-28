8598bb83dd8edb059d58e34de3d5c6b6b9c66923
code-chelangat <caroroline.chelangat@student.moringaschool.com>
2022-08-28 19:10:40 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, string){
+    return drivers.filter((driver)=>{
+        return driver.toLowerCase().includes(string.toLowerCase())
+    }) 
+}
+function  fuzzyMatch(drivers, string){
+    return drivers.filter(driver=>{
+        return driver.toLowerCase().startsWith(string.toLowerCase())
+    })
+}
+function matchName(driverList,string){
+return driverList.filter(driver=>{
+    return driver.name.toLowerCase().includes(string.toLowerCase())
+})
+}
